529cd38ac758e53f43547767711135a28e261179
Chaim Lev Ari <chiptus@gmail.com>
2019-06-15 21:10:50 +0300

    add read more button

--- src/components/campaign-card.js
+++ src/components/campaign-card.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 
+const readMoreButtonStyle = {
+  background: 'none',
+  border: 'none',
+  color: '#007bff',
+  marginLeft: '0.5em',
+};
+
 export function CampaignCard({ campaign, ...props }) {
+  const [isCollapsed, description, toggleCollapsed] = useCollapsedText(campaign.description || '');
   return (
     <Card {...props}>
       <Card.Img variant="top" src={campaign.imageUrl} style={{ maxHeight: '10em', objectFit: 'contain' }} />
@@ -9,8 +17,39 @@ export function CampaignCard({ campaign, ...props }) {
         <Card.Title>
           <a href={campaign.url}>{campaign.title}</a>
         </Card.Title>
-        <Card.Text>{campaign.description}</Card.Text>
+        <Card.Text>
+          {isCollapsed && description.short ? (
+            <span className="short-text">
+              {description.short}...
+              <button className="read-more-button" style={readMoreButtonStyle} onClick={toggleCollapsed}>
+                Read more
+              </button>
+            </span>
+          ) : (
+            <span className="full-text">{description.full}</span>
+          )}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
+}
+
+function useCollapsedText(text, maxLength = 200) {
+  const [isCollapsed, setCollapsed] = useState(true);
+
+  function toggleCollapsedState() {
+    setCollapsed(!isCollapsed);
+  }
+
+  let short = text.substring(0, maxLength);
+  if (text[maxLength + 1] !== ' ') {
+    short = short.substring(0, short.lastIndexOf(' '));
+  }
+
+  const value = {
+    full: text,
+    short,
+  };
+
+  return [isCollapsed, value, toggleCollapsedState];
 }
